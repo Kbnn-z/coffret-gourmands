6c6df7695555325d24ac6f034826dee890a213c0
_Kbrønn <115071427+Kbnn-z@users.noreply.github.com>
2023-11-03 22:32:03 +0100

    Add files via upload

---
+++ barre_recherche.js
@@ -0,0 +1,45 @@
+function barreRecherche(){
+    var a = document.getElementById("search").value;
+
+    if(a === "Accueil"){
+        window.open("index.html");
+    }
+
+    if(a === "Coffrets"){
+        window.open("page_coffret.html");
+    }
+
+    if(a === "A propos"){
+        window.open("page_apropos.html");
+    }
+
+    if(a === "Panier"){
+        window.open("page_panier.html");
+    }
+
+    if(a === "Mentions légales"){
+        window.open("mentions_legales.html");
+    }
+
+    if(a === "Contact"){
+        window.open("page_contact.html");
+    }
+
+    if(a === "Protection de données"){
+        window.open("politique-de-protection-de-vos-donnees-personnelles.pdf");
+    }
+
+    if(a === "Engagement"){
+        window.open("page_engagements.html");
+    }
+
+    if(a === "coffret anniversaire"){
+        window.open("page_coffret_anniv.html");
+    }
+
+    if(a === "coffret anniversaire"){
+        window.open("page_coffret_anniv.html");
+    }
+
+
+}
