c2eb1fcd410aaa2aea71748e0ec7c1615813b7f2
_Kbrønn <115071427+Kbnn-z@users.noreply.github.com>
2023-11-05 19:36:33 +0100

    Add files via upload

--- barre_recherche.js
+++ barre_recherche.js
@@ -33,13 +33,43 @@ function barreRecherche(){
         window.open("page_engagements.html");
     }
 
-    if(a === "coffret anniversaire"){
+    if(a === "Coffret anniversaire"){
         window.open("page_coffret_anniv.html");
     }
 
-    if(a === "coffret anniversaire"){
-        window.open("page_coffret_anniv.html");
+    if(a === "Coffret apéro"){
+        window.open("page_coffret_apero.html");
     }
 
+    if(a === "Coffret goûter"){
+        window.open("page_coffret_gouter.html");
+    }
 
+    if(a === "Coffret déjeuner"){
+        window.open("page_coffret_dej.html");
+    }
+
+    if(a === "Coffret petit déjeuner"){
+        window.open("page_coffret_petitdej.html");
+    }
+
+    if(a === "Coffret barbecue"){
+        window.open("page_coffret_bbq.html");
+    }
+
+    if(a === "Coffret diner"){
+        window.open("page_coffret_diner.html");
+    }
+
+    if(a === "Coffret de luxe"){
+        window.open("page_coffret_luxe.html");
+    }
+
+    if(a === "Coffret étudiant"){
+        window.open("page_coffret_etudiant.html");
+    }
+
+    if(a === "Coffret mystère"){
+        window.open("page_coffret_surprise.html");
+    }
 }
